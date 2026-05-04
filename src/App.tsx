089be157ac5407b20963dc101ea1f8cfaecb98ec
KseniaGKS <07gks05@gmail.com>
2026-05-04 16:47:29 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,12 @@ function App() {
   <div style={{ 
     width: "100vw", 
     height: "100vh", 
-    display: "flex", 
-    justifyContent: "center", 
-    alignItems: "center", 
-    backgroundColor: "#1a1a1a",
+    backgroundColor: "#1a1a1a" 
   }}>
-    <div style={{ 
-      // Соотношение сторон задаем внешнему блоку
-      aspectRatio: "9 / 16", 
-      height: "100%", 
-      maxHeight: "100vh",
-      maxWidth: "100vw",
-      display: "flex" 
-    }}>
-      <Unity
-        unityProvider={unityProvider}
-        style={{ 
-          width: "100%", 
-          height: "100%",
-          boxShadow: "0 0 20px rgba(0,0,0,0.5)" 
-        }}
-      />
-    </div>
+    <Unity
+      unityProvider={unityProvider}
+      style={{ width: "100%", height: "100%" }}
+    />
   </div>
 );
 }
