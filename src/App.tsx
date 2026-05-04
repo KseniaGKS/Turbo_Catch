a2cee796e20d8757dfa84895c5b4626678bace33
KseniaGKS <07gks05@gmail.com>
2026-05-04 17:25:14 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,32 +10,16 @@ function App() {
 
   return (
     <div style={{ 
-      width: "100vw", 
-      height: "100vh", 
-      display: "flex", 
-      justifyContent: "center", 
-      alignItems: "center", 
-      backgroundColor: "#1a1a1a",
-      margin: 0,
-      overflow: "hidden"
+    height: "100vh",
+    width: "412px", 
+    maxWidth: "100vw", 
+    position: "relative",
+    display: "flex"
     }}>
-      <div style={{ 
-        // Хитрость здесь: ширина всегда будет 9/16 от высоты экрана
-        height: "100vh",
-        width: "calc(100vh * 9 / 16)", 
-        maxWidth: "100vw", // Чтобы на узких телефонах не вылезало за края
-        position: "relative",
-        display: "flex"
-      }}>
-        <Unity
-          unityProvider={unityProvider}
-          style={{ 
-            width: "100%", 
-            height: "100%",
-            display: "block"
-          }}
-        />
-      </div>
+    <Unity
+    unityProvider={unityProvider}
+    style={{ width: "100%", height: "100%" }}
+    />
     </div>
   );
 }
