1bd8d81058be02e0759e926566496df5c857d753
KseniaGKS <07gks05@gmail.com>
2026-05-04 16:51:38 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,26 @@ function App() {
   });
 
   return (
-  <div style={{ 
-    width: "100vw", 
-    height: "100vh", 
-    backgroundColor: "#1a1a1a" 
-  }}>
-    <Unity
-      unityProvider={unityProvider}
-      style={{ width: "100%", height: "100%" }}
-    />
-  </div>
-);
+    <div style={{ 
+      width: "100vw", 
+      height: "100vh", 
+      display: "flex", 
+      justifyContent: "center", 
+      alignItems: "center", 
+      backgroundColor: "#1a1a1a",
+      overflow: "hidden"
+    }}>
+      <div style={{ 
+        height: "100%",
+        aspectRatio: "9 / 16", 
+        display: "flex"
+      }}>
+        <Unity
+          unityProvider={unityProvider}
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
+    </div>
+  );
 }
 export default App;
