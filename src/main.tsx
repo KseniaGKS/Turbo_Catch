320dc8449ec2634a449f08290bbc7bcf21f42f0f
KseniaGKS <07gks05@gmail.com>
2026-05-04 16:30:09 +0500

    deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import bridge from '@vkontakte/vk-bridge';
+
+// Инициализация ВК
+bridge.send("VKWebAppInit");
 
 createRoot(document.getElementById('root')!).render(
     <App />,
